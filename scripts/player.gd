0a1ba4407a4f313c205ed760a74b1b478e738f8c
Danillo Gomes <30347668+danillog@users.noreply.github.com>
2025-04-28 07:18:30 -0300

    feat/collision (#2)

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,13 @@
 extends Area2D
 
+signal hit
 const SPEED := 400
-var screen_size
+@onready var screen_size = get_viewport_rect().size
 @onready  var anim = $anim
+@onready var collision = $CollisionShape2D
 
 func _ready():
-	screen_size = get_viewport_rect().size
+	hide()
 
 func _process(delta):
 	var velocity = Input.get_vector("move_left","move_right","move_up","move_down")
@@ -13,7 +15,30 @@ func _process(delta):
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * SPEED
 		
+	if velocity.x != 0:
+		anim.play("move")
+	elif velocity.y < 0:
+		anim.play("move_up")
+	elif velocity.y > 0:
+		anim.play("move_down")
+	else:
+		anim.play("idle")
+	
+# ternar if in godot 
+	anim.flip_h = true if velocity.x < 0 else false
+	
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
 	
 	
+
+#verify  player collision
+func _on_body_entered(body: Node2D) -> void:
+	hide()
+	hit.emit()
+	collision.set_deferred("disabled", true)
+
+func start_pos(pos):
+	position = pos
+	show()
+	collision.disabled = false
